47a4e895f005bb57d77376934fc1c0aaf69e9e1a
Gaya Kessler <gaya.kessler@gmail.com>
2018-03-15 11:13:45 +0100

    update typescript declaration files

--- factory.d.ts
+++ factory.d.ts
@@ -1,9 +1,7 @@
-// Type definitions for vue-plotly.js
+// Type definitions for factory.js
 // Project: Vue Plotly.js
 // Definitions by: BrewBlox
 
 import { Component } from 'vue/types/options';
 
-export const Plotly: Component;
-
-export default Plotly;
+export default function(plotly: any): Component;
