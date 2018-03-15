da62eeed767a1cdb9d85bfc52e684d69924bfda7
Gaya Kessler <gaya.kessler@gmail.com>
2018-03-15 08:03:18 +0100

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true
+  },
+  extends: ['plugin:vue/strongly-recommended', 'airbnb-base'],
+  // required to lint *.vue files
+  plugins: [
+    'vue',
+  ],
+};
